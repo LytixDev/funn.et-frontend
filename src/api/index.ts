f48961ed0933a89b752c0a7d4dbf7a93360c3a31
Nicolai Hollup Brand <nicolai.brand@ntnu.no>
2023-03-18 15:53:44 +0100

    feat: ListingCard component

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,3 +5,7 @@ export { ApiError } from './core/ApiError';
 export { CancelablePromise, CancelError } from './core/CancelablePromise';
 export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
+
+export { ListingDTO } from './models/ListingDTO';
+
+export { ListingControllerService } from './services/ListingControllerService';
