f36c5f2d6d070852cc7eae531f3ad598a8512cd1
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-19 14:00:07 +0100

    config file for cypress and prettier

--- cypress/e2e/NotFoundView/notFoundView.cy.ts
+++ cypress/e2e/NotFoundView/notFoundView.cy.ts
@@ -1,30 +1,30 @@
-const root = Cypress.config("e2e").baseUrl;
+const root = Cypress.config().baseUrl;
 
-describe("Integration tests for the 404 not found page", () => {
-    it("Clicking go back button takes you home if home was visited last", () => {
-        cy.visit("/");
-        cy.visit("/this-page-does-not-exist");
-        cy.get('[data-testid="goBack"]').click();
-        cy.url().should("eq", root);
-    })
+describe('Integration tests for the 404 not found page', () => {
+  it('Clicking go back button takes you home if home was visited last', () => {
+    cy.visit('/');
+    cy.visit('/this-page-does-not-exist');
+    cy.get('[data-testid="goBack"]').click();
+    cy.url().should('eq', root);
+  });
 
-    it("Clicking go back button takes you to the last visited page", () => {
-        cy.visit("/about");
-        cy.visit("/this-page-does-not-exist");
-        cy.get('[data-testid="goBack"]').click();
-        cy.url().should("eq", root + "about");
-    })
+  it('Clicking go back button takes you to the last visited page', () => {
+    cy.visit('/about');
+    cy.visit('/this-page-does-not-exist');
+    cy.get('[data-testid="goBack"]').click();
+    cy.url().should('eq', root + 'about');
+  });
 
-    it("Clicking go home button takes you home", () => {
-        cy.visit("/this-page-does-not-exist");
-        cy.get('[data-testid="goHome"]').click();
-        cy.url().should("eq", root);
-    })
+  it('Clicking go home button takes you home', () => {
+    cy.visit('/this-page-does-not-exist');
+    cy.get('[data-testid="goHome"]').click();
+    cy.url().should('eq', root);
+  });
 
-    it("Clicking go home button takes you home when last was about", () => {
-        cy.visit("/about");
-        cy.visit("/this-page-does-not-exist");
-        cy.get('[data-testid="goHome"]').click();
-        cy.url().should("eq", root);
-    })
+  it('Clicking go home button takes you home when last was about', () => {
+    cy.visit('/about');
+    cy.visit('/this-page-does-not-exist');
+    cy.get('[data-testid="goHome"]').click();
+    cy.url().should('eq', root);
+  });
 });
