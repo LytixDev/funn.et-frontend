736c30495110d45d67719789d71aa8454fd5fbcc
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-21 16:05:13 +0100

    create signout page to clear out user info

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,7 +30,7 @@ const routes = [
       {
         path: '/signout',
         name: 'signout',
-        component: () => import('@/views/LoginUserView.vue'),
+        component: () => import('@/views/SignoutUserView.vue'),
       },
       {
         path: '/user/:id',
