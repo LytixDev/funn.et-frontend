2e478aec4498de3fb7ec63b33e102e5da443c1c5
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-03-22 14:20:56 +0100

    fix: setup inside script tag

--- cypress/e2e/Authentication/LoginUserView.cy.ts
+++ cypress/e2e/Authentication/LoginUserView.cy.ts
@@ -1,29 +1,29 @@
-const apiUrl = Cypress.env("apiUrl");
+const apiUrl = Cypress.env('apiUrl');
 
-describe("Test user using the login page", () => {
-    beforeEach(() => {
-        cy.clearLocalStorage();
-        cy.visit("/login");
+describe('Test user using the login page', () => {
+  beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.visit('/login');
+  });
+
+  it('Test user logging in saves a token in localstorage', () => {
+    const testToken = 'TestTokenFromMockBackend';
+    const username = 'TestUser';
+    cy.intercept('POST', `${apiUrl}api/v1/public/token`, {
+      statusCode: 200,
+      body: testToken,
     });
 
-    it("Test user logging in saves a token in localstorage", () => {
-        const testToken = 'TestTokenFromMockBackend'
-        const username = 'TestUser';
-        cy.intercept("POST", `${apiUrl}api/v1/public/token`, {
-            statusCode: 200,
-            body: testToken
-        });
-
-        cy.get('input[data-testid="username"]', {timeout: 60000}).should('exist').type(username);
-        cy.get('input[data-testid="password"]').type("testPassword123");
-        cy.get('button[data-testid="login-user-button"]').click();
-        cy.getAllLocalStorage().then((result) => {
-            expect(result).to.deep.equal({
-                'http://localhost:4173': {
-                    token: testToken,
-                    username: username
-                }
-            });
-        });
+    cy.get('input[data-testid="username"]', { timeout: 60000 }).should('exist').type(username);
+    cy.get('input[data-testid="password"]').type('testPassword123');
+    cy.get('button[data-testid="login-user-button"]').click();
+    cy.getAllLocalStorage().then((result) => {
+      expect(result).to.deep.equal({
+        'http://localhost:4173': {
+          token: testToken,
+          username: username,
+        },
+      });
     });
-})
+  });
+});
--- src/views/Authenitcation/RegisterUserView.vue
+++ src/views/Authenitcation/RegisterUserView.vue
@@ -54,14 +54,12 @@
   <ErrorBox v-if="errorBoxMsg" v-model="errorBoxMsg" />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import FormButton from '@/components/Form/FormButton.vue';
 import FormInput from '@/components/Form/FormInput.vue';
 import { FormInputTypes } from '@/enums/FormEnums';
-import BasePageView from '@/views/BasePageView.vue';
 import { useForm, useField, FieldContext } from 'vee-validate';
 import { object as yupObject, string as yupString } from 'yup';
-import { defineComponent } from 'vue';
 import { computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { PublicUserControllerService, RegisterDTO } from '@/api';
@@ -70,105 +68,80 @@ import { useUserInfoStore } from '@/stores/UserStore';
 import { TokenControllerService, AuthenticateDTO } from '@/api';
 import router from '@/router';
 
-export default defineComponent({
-  components: {
-    BasePageView,
-    FormInput,
-    FormButton,
-    ErrorBox,
-  },
-  setup() {
-    const userStore = useUserInfoStore();
-    const { t } = useI18n();
-    let errorBoxMsg = ref<string>('');
+const userStore = useUserInfoStore();
+const { t } = useI18n();
+let errorBoxMsg = ref<string>('');
 
-    const schema = yupObject({
-      username: yupString()
-        .min(3, computed(() => t?.('UserForm.Error.usernameMin')).value)
-        .max(32, computed(() => t?.('UserForm.Error.usernameMax')).value)
-        .required(computed(() => t?.('UserForm.Error.usernameRequired')).value),
-      email: yupString()
-        .required(computed(() => t?.('UserForm.Error.emailRequired')).value)
-        .email(computed(() => t?.('UserForm.Error.emailInvalid')).value),
-      firstName: yupString().required(computed(() => t?.('UserForm.Error.firstNameRequired')).value),
-      lastName: yupString().required(computed(() => t?.('UserForm.Error.lastNameRequired')).value),
-      password: yupString()
-        .required(computed(() => t?.('UserForm.Error.passwordRequired')).value)
-        .min(8, computed(() => t?.('UserForm.Error.passwordMin')).value)
-        .test('isValidPass', computed(() => t?.('UserForm.Error.passwordInvalid')).value, (value) => {
-          const hasUpperCase = /[A-Z]/.test(value);
-          const hasLowerCase = /[a-z]/.test(value);
-          const hasNumber = /[0-9]/.test(value);
-          return hasUpperCase && hasLowerCase && hasNumber;
-        }),
-    });
+const schema = computed(() =>
+  yupObject({
+    username: yupString()
+      .min(3, t('UserForm.Error.usernameMin'))
+      .max(32, t('UserForm.Error.usernameMax'))
+      .required(t('UserForm.Error.usernameRequired')),
+    email: yupString().required(t('UserForm.Error.emailRequired')).email(t('UserForm.Error.emailInvalid')),
+    firstName: yupString().required(t('UserForm.Error.firstNameRequired')),
+    lastName: yupString().required(t('UserForm.Error.lastNameRequired')),
+    password: yupString()
+      .required(t('UserForm.Error.passwordRequired'))
+      .min(8, t('UserForm.Error.passwordMin'))
+      .test('isValidPass', t('UserForm.Error.passwordInvalid'), (value) => {
+        const hasUpperCase = /[A-Z]/.test(value);
+        const hasLowerCase = /[a-z]/.test(value);
+        const hasNumber = /[0-9]/.test(value);
+        return hasUpperCase && hasLowerCase && hasNumber;
+      }),
+  }),
+);
 
-    const { handleSubmit, errors } = useForm({
-      validationSchema: schema,
-    });
-
-    const submit = handleSubmit(async (values) => {
-      console.log(values);
-
-      const createUserPayload: RegisterDTO = {
-        username: values.username,
-        email: values.email,
-        firstName: values.firstName,
-        lastName: values.lastName,
-        password: values.password,
-      };
-
-      await PublicUserControllerService.createUser({ requestBody: createUserPayload })
-        .then(() => {
-          let auth: AuthenticateDTO = { username: values.username, password: values.password };
-
-          TokenControllerService.generateToken({ requestBody: auth })
-            .then((token) => {
-              if (token == null || token == undefined) {
-                errorBoxMsg.value = 'Internal server error, please try again later.';
-                return;
-              }
-
-              userStore.setUserInfo({ token: token, username: values.username });
-              router.push({ name: 'home' });
-            })
-            .catch((authError) => {
-              errorBoxMsg.value = authError.body;
-              console.log(authError.body);
-            });
-        })
-        .catch((error) => {
-          errorBoxMsg.value = error.body;
-          console.log(error.body, error);
-          //TODO: handle error
-          //switch (
-          //  error.body as string
-          //) {
-          //}
-        });
-    });
-
-    /* form values */
-    const { value: username } = useField('username') as FieldContext<string>;
-    const { value: email } = useField('email') as FieldContext<string>;
-    const { value: firstName } = useField('firstName') as FieldContext<string>;
-    const { value: lastName } = useField('lastName') as FieldContext<string>;
-    const { value: password } = useField('password') as FieldContext<string>;
-
-    return {
-      username,
-      email,
-      firstName,
-      lastName,
-      password,
-      schema,
-      submit,
-      errors,
-      errorBoxMsg,
-      FormInputTypes,
-    };
-  },
+const { handleSubmit, errors } = useForm({
+  validationSchema: schema,
 });
+
+const submit = handleSubmit(async (values) => {
+  const createUserPayload: RegisterDTO = {
+    username: values.username,
+    email: values.email,
+    firstName: values.firstName,
+    lastName: values.lastName,
+    password: values.password,
+  };
+
+  await PublicUserControllerService.createUser({ requestBody: createUserPayload })
+    .then(() => {
+      let auth: AuthenticateDTO = { username: values.username, password: values.password };
+
+      TokenControllerService.generateToken({ requestBody: auth })
+        .then((token) => {
+          if (token == null || token == undefined) {
+            errorBoxMsg.value = 'Internal server error, please try again later.';
+            return;
+          }
+
+          userStore.setUserInfo({ token: token, username: values.username });
+          router.push({ name: 'home' });
+        })
+        .catch((authError) => {
+          errorBoxMsg.value = authError.body;
+          console.log(authError.body);
+        });
+    })
+    .catch((error) => {
+      errorBoxMsg.value = error.body;
+      console.log(error.body, error);
+      //TODO: handle error
+      //switch (
+      //  error.body as string
+      //) {
+      //}
+    });
+});
+
+/* form values */
+const { value: username } = useField('username') as FieldContext<string>;
+const { value: email } = useField('email') as FieldContext<string>;
+const { value: firstName } = useField('firstName') as FieldContext<string>;
+const { value: lastName } = useField('lastName') as FieldContext<string>;
+const { value: password } = useField('password') as FieldContext<string>;
 </script>
 
 <style scoped></style>
