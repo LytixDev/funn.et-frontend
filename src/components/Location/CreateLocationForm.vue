40ac9db4f945ddd391a35d676d122edd2941b219
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-23 19:15:07 +0100

    Add marker to leaflet and search for address

--- src/components/Location/CreateLocationForm.vue
+++ src/components/Location/CreateLocationForm.vue
@@ -8,8 +8,8 @@
       v-model="address"
       data-testid="search-input" />
 
-      <div>
-        <form-drop-down-list
+    <div>
+      <form-drop-down-list
         label-id="locations-label"
         field-id="locations-select"
         :label-text="$t('CreateLocationForm.locationsSelectLabel') + ': '"
@@ -18,19 +18,20 @@
         field-name="locations-select"
         data-testid="locations-select"
         :error="errors?.select" />
-        <p>{{ $t('CreateLocationForm.numberOfLocationsDisplaying') }}: {{ locationList.length }}</p>
-      </div>
+      <p>{{ $t('CreateLocationForm.numberOfLocationsDisplaying') }}: {{ locationList.length }}</p>
+    </div>
     <form-button
       button-id="submit-button"
       :button-text="$t('CreateLocationForm.submitButton')"
-      data-testid="submit-button" />
+      data-testid="submit-button"
+      @click="submit" />
   </form>
-  <p>{{ errorMessage }}</p>
+  <p>{{ errorMessage }} {{ selectField }}</p>
+  <location-map :center="{ lat: mapCenterLat, lon: matCenterLon }" :marker-coords="markerCoords" :zoom="zoom" />
 </template>
 
 <script setup lang="ts">
 import { computed, ref, watchEffect } from 'vue';
-import { FormInputTypes } from '@/enums/FormEnums';
 import 'leaflet/dist/leaflet.css';
 import FormInput from '@/components/Form/FormInput.vue';
 import { DefaultService, OutputAdresse, OutputAdresseList } from '@/api/geonorge';
@@ -41,14 +42,43 @@ import { DropDownItem } from '@/types/FormTypes';
 import { object as yupObject, string as yupString } from 'yup';
 import { useForm, useField, FieldContext } from 'vee-validate';
 import { useI18n } from 'vue-i18n';
+import LocationMap from '@/components/Location/LocationMap.vue';
 
 const address = ref('');
-const chosenLocation = ref('');
 
 const errorMessage = ref('');
 
 const locationOptions = ref([] as DropDownItem[]);
 const locationList = ref([] as OutputAdresse[]);
+
+const mapCenterLat = ref(0);
+const matCenterLon = ref(0);
+
+const selectedLocation = computed(() => {
+  if (locationList.value.length === 0) {
+    return undefined;
+  }
+  if (selectField.value === undefined) {
+    selectField.value = locationList.value.at(0)?.adressetekst?.toString()!!;
+    return locationList.value[0];
+  }
+  const location = locationList.value.at(
+    locationList.value.map((location) => location.adressetekst?.toString()).indexOf(selectField.value),
+  )!!;
+  selectField.value = location.adressetekst?.toString()!!;
+  return location;
+});
+const markerCoords = computed(() => {
+  const lat = selectedLocation.value?.representasjonspunkt?.lat;
+  const lon = selectedLocation.value?.representasjonspunkt?.lon;
+  if (lat && lon) {
+    zoom.value = 16;
+    return { lat, lon };
+  }
+  return undefined;
+});
+
+const zoom = ref(2);
 
 const { t } = useI18n();
 
@@ -63,22 +93,25 @@ const { handleSubmit, errors } = useForm({
 });
 
 const submit = handleSubmit(async (values) => {
-  console.log(values);
-  const location = locationList.value.at(locationList.value.map(location => location.adressekode?.toString()).indexOf(values.select))!!;
+  if (selectedLocation.value === undefined) {
+    errorMessage.value = t('CreateLocationForm.Error.AddressRequired');
+  }
   LocationControllerService.createLocation({
     requestBody: {
-      address: location.adressetekst!!,
-      latitude: location.representasjonspunkt?.lat!!,
-      longitude: location.representasjonspunkt?.lon!!,
-      postCode: +(location.postnummer!!),
-      city: location.poststed!!,
-    }
-  }).then(() => {
-    console.log('Location created');
-  }).catch((error: any) => {
-    errorMessage.value = error.message;
-  });
-})
+      address: selectedLocation.value?.adressetekst!!,
+      latitude: selectedLocation.value?.representasjonspunkt?.lat!!,
+      longitude: selectedLocation.value?.representasjonspunkt?.lon!!,
+      postCode: +selectedLocation.value?.postnummer!!,
+      city: selectedLocation.value?.poststed!!,
+    },
+  })
+    .then(() => {
+      console.log('Location created');
+    })
+    .catch((error: any) => {
+      errorMessage.value = error.message;
+    });
+});
 
 const { value: selectField } = useField('select') as FieldContext<string>;
 
@@ -100,7 +133,7 @@ watchEffect(async () => {
   }
   locationList.value = locations.adresser!!;
   locationOptions.value = locationList.value.map((location: OutputAdresse) => ({
-    value: location.adressekode!!.toString(),
+    value: location.adressetekst!!.toString(),
     displayedValue: `${location.adressetekst} ${location.postnummer} ${location.poststed}`,
   }))!!;
   return;
---
+++ src/components/Location/LocationMap.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="height: 600px; width: 800px">
+    <l-map
+      ref="map"
+      :zoom="zoom"
+      :center="validMarker ? [markerCoords?.lat, markerCoords?.lon] : [center.lat, center.lon]">
+      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap">
+      </l-tile-layer>
+      <l-marker v-if="validMarker" :lat-lng="markerCoords" />
+    </l-map>
+  </div>
+</template>
+
+<script lang="ts">
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  props: {
+    center: {
+      type: Object as () => { lat: number; lon: number },
+      required: false,
+      default: () => ({ lat: 47.41322, lon: -1.219482 }),
+    },
+    zoom: {
+      type: Number,
+      required: false,
+      default: 3,
+    },
+    markerCoords: {
+      type: Object as () => { lat: number; lon: number },
+      required: false,
+    },
+  },
+  computed: {
+    validMarker() {
+      return this.markerCoords?.lat && this.markerCoords?.lon;
+    },
+  },
+});
+</script>
+
+<style scoped></style>
