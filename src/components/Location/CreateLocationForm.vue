a79e6fa62a8d34c1d4f2ac74e5cf121c17c7df11
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-23 14:47:15 +0100

    Create search for address in geonorge

--- src/components/Location/CreateLocationForm.vue
+++ src/components/Location/CreateLocationForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="">
     <!-- Inputs for creating location -->
     <form-input
       labelId="location-address-label"
@@ -20,21 +20,67 @@
       :labelText="$t('CreateLocationForm.cityLabel') + ': '"
       v-model="city"
       data-testid="city-input" />
+    <form-drop-down-list
+      label-id="locations-label"
+      field-id="locations-select"
+      :label-text="$t('CreateLocationForm.locationsSelectLabel') + ': '"
+      v-model="chosenLocation"
+      :field-options="locationOptions"
+      field-name="locations-select"
+      data-testid="locations-select" />
+      <form-button
+        button-id="submit-button"
+        :button-text="$t('CreateLocationForm.submitButton')"
+        data-testid="submit-button" />
   </form>
+  <p>{{ errorMessage }}</p>
 </template>
 
 <script setup lang="ts">
-import { LocationControllerService } from '@/api';
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { FormInputTypes } from '@/enums/FormEnums';
+import 'leaflet/dist/leaflet.css';
+import FormInput from '@/components/Form/FormInput.vue';
+import { DefaultService, OutputAdresse, OutputAdresseList } from '@/api/geonorge';
+import FormDropDownList from '@/components/Form/FormDropDownList.vue';
+import FormButton from '@/components/Form/FormButton.vue';
+import { DropDownItem } from '@/types/FormTypes';
 
 const address = ref('');
-const latitude = ref(undefined as number | undefined);
-const longitude = ref(undefined as number | undefined);
-const postCode = ref(undefined as number | undefined);
-const city = ref('');
+const postCode = ref(undefined as string | undefined);
+const city = ref(undefined as string | undefined);
+const chosenLocation = ref('');
 
+const errorMessage = ref('');
 
+const locationOptions = ref([] as DropDownItem[]);
+const locationList = ref([] as OutputAdresse[]);
+
+watchEffect(async () => {
+  if (address.value === '') {
+    return;
+  }
+  let locations: OutputAdresseList | undefined;
+  try {
+    locations = await DefaultService.getSok({
+      sok: address.value ?? '',
+      postnummer: postCode.value === '' ? undefined : postCode.value,
+      poststed: city.value === '' ? undefined : city.value,
+      fuzzy: true,
+    });
+  } catch (error: any) {
+    errorMessage.value = error.message;
+  }
+  if (locations?.adresser === undefined || locations.metadata?.totaltAntallTreff === 0) {
+    return;
+  }
+  locationList.value = locations.adresser!!;
+  locationOptions.value = locationList.value.map((location: OutputAdresse) => ({
+    value: location.adressekode!!.toString(),
+    displayedValue: location.adressetekst!!,
+  }))!!;
+  return;
+});
 </script>
 
 <style scoped></style>
--- src/components/User/UserDetail.vue
+++ src/components/User/UserDetail.vue
@@ -6,6 +6,7 @@
     <p>{{ user.firstName }} {{ user.lastName }}</p>
     <p>{{ user.email }}</p>
   </div>
+  <create-location-form />
 </template>
 
 <script setup lang="ts">
