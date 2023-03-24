0b1883e4b698061d348e16f7cce3e222889c9ea8
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-03-24 18:20:15 +0100

    api generate

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,6 +7,7 @@ export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { AuthenticateDTO } from './models/AuthenticateDTO';
+export type { ChatDTO } from './models/ChatDTO';
 export type { ExceptionResponse } from './models/ExceptionResponse';
 export { FilterRequest } from './models/FilterRequest';
 export type { ImageResponseDTO } from './models/ImageResponseDTO';
@@ -14,11 +15,14 @@ export { ListingCreateDTO } from './models/ListingCreateDTO';
 export { ListingDTO } from './models/ListingDTO';
 export type { LocationCreateDTO } from './models/LocationCreateDTO';
 export type { LocationResponseDTO } from './models/LocationResponseDTO';
+export type { MessageDTO } from './models/MessageDTO';
 export type { RegisterDTO } from './models/RegisterDTO';
 export type { SearchRequest } from './models/SearchRequest';
 export { SortRequest } from './models/SortRequest';
 export { UserDTO } from './models/UserDTO';
+export type { UserPatchDTO } from './models/UserPatchDTO';
 
+export { ChatControllerService } from './services/ChatControllerService';
 export { ImageControllerService } from './services/ImageControllerService';
 export { ListingControllerService } from './services/ListingControllerService';
 export { LocationControllerService } from './services/LocationControllerService';
