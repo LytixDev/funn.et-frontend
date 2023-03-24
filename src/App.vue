170a13f760e7f16b9c203b362b2d3bcdb75ef6d4
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-24 03:31:22 +0100

    Create Error Boundary Catcher and translations and add to app and base page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
-  <router-view />
+  <error-boundary-catcher>
+    <template #default>
+      <router-view />
+    </template>
+  </error-boundary-catcher>
 </template>
+
+<script setup lang="ts">
+import ErrorBoundaryCatcher from '@/components/Exceptions/ErrorBoundaryCatcher.vue';
+</script>
 
 <style scoped></style>
---
+++ src/components/Exceptions/ErrorBoundaryCatcher.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="error-boundary">
+    <slot v-if="errorMessage" name="fallback">
+      <div>
+        <h3>{{ $t('Exceptions.ExceptionOccurred') }}</h3>
+        <p>{{ $t(`Exceptions.${errorMessage}`, errorContext) }}</p>
+      </div>
+    </slot>
+    <slot v-else />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ApiError } from '@/api';
+import { AxiosError } from 'axios';
+import { onErrorCaptured, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const errorMessage = ref('');
+const errorContext = ref({} as { [key: string]: string });
+
+onErrorCaptured((err, _vm, _info): boolean => {
+  errorContext.value = {};
+  if (err instanceof ApiError) {
+    if (err.status == 401) {
+      errorMessage.value = err.body.detail;
+      router.push({ name: 'Login' });
+      return false;
+    }
+    errorMessage.value = err.body.detail;
+  } else if (err instanceof AxiosError) {
+    console.log('AxiosError: ' + err.code);
+    errorMessage.value = err.code!!;
+  } else if (err instanceof Error) {
+    errorMessage.value = 'ErrorWithContext';
+    errorContext.value = { message: err === undefined || err === null ? 'ContextErrorMessage' : err.message };
+  }
+  return false;
+});
+</script>
