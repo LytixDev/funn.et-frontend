856de029870047d1e1faf6d712763d6d9124cce0
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-26 13:51:10 +0200

    Add favorite listing api call for favorites

--- src/components/User/FavoriteListingsByUser.vue
+++ src/components/User/FavoriteListingsByUser.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    <h4>This is my favorite passages</h4>
+    <listing-list :listings="listings" />
   </div>
+  <error-box :error="errorMessage" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ListingDTO, ListingControllerService, ApiError } from '@/api/backend';
+import ListingList from '@/components/Listing/ListingList.vue';
+import { AxiosError } from 'axios';
+import { ref } from 'vue';
+import ErrorBox from '../Exceptions/ErrorBox.vue';
+
+const errorMessage = ref('');
+const listings = ref([] as ListingDTO[] | undefined);
+
+try {
+  listings.value = await ListingControllerService.getFavoriteListings();
+} catch (error) {
+  if (error instanceof AxiosError) {
+    errorMessage.value = error.code!!;
+  }
+  if (error instanceof ApiError) {
+    errorMessage.value = error.body.detail;
+  } else {
+    errorMessage.value = 'ErrorWithContext';
+  }
+}
+</script>
 
 <style scoped></style>
