26eac10fa4bc052a1217333fd5222fd994137391
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-03-26 17:41:06 +0200

    lol

--- cypress/e2e/Listings/ListingListView.cy.ts
+++ cypress/e2e/Listings/ListingListView.cy.ts
@@ -1,13 +1,13 @@
 apiUrl = Cypress.env('apiUrl');
 
 describe('Listing list view', () => {
-  it('The listing list should display next pagination button with 20 listings', () => {
+  it('The listing list should display next pagination button with 24 listings', () => {
     const listings = [];
     for (let i = 0; i < 24; i++) {
       listings.push({
         id: i,
         briefDescription: `Listing ${i}`,
-        category: 'ELECTRONICS',
+        category: 0,
         price: 100,
         title: `Listing ${i}`,
       });
