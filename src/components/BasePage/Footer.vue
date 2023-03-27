6e813c6ef7aa323c40934a629caec32f16d85f65
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-03-27 11:58:23 +0200

    add copyright to footer

--- src/components/BasePage/Footer.vue
+++ src/components/BasePage/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <p>Footer</p>
+  <p>© Copyright 2023 | Carl J. M. H. Gützkow, Callum Gran, Thomas Svendal, Nicolai H. Brand</p>
   </footer>
 </template>
 
@@ -9,4 +9,8 @@ import { defineComponent } from 'vue';
 export default defineComponent({});
 </script>
 
-<style scoped></style>
+<style scoped>
+p {
+padding-top: 1rem;
+}
+</style>
